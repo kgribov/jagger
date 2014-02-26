771e0a91c959edf74e8556ef2a34b2a83fed2ecf
kgribov <kgribov@griddynamics.com>
2014-02-26 16:54:04 +0400

    Add documentation for schema and QueryProcessor

--- chassis/core/src/main/java/com/griddynamics/jagger/invoker/QueryProcessor.java
+++ chassis/core/src/main/java/com/griddynamics/jagger/invoker/QueryProcessor.java
@@ -1,11 +1,21 @@
 package com.griddynamics.jagger.invoker;
 
-/**
- * Created by kgribov on 2/18/14.
- */
+/** An object, which can process query and endpoint before and after invocation
+ * @author Gribov Kirill
+ * @n
+ * @par Details:
+ * @details QueryProcessor decides what query and endpoint will be executed next @n
+ * @n */
 public interface QueryProcessor {
 
+
+    /** Method is executed before invocation
+    * @param stepInfo - contains query and endpoint, which were passed from previous step or query-distributor
+    * @return query and endpoint for current invocation */
     StepInfo preProcess(StepInfo stepInfo);
 
+    /** Method is executed after invocation
+     * @param currentResult - contains invocation query, endpoint and result
+     * @return query and endpoint for next invocation step. If it equals null, next step will be not executed */
     StepInfo postProcess(StepResult currentResult);
 }
